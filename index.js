3011703121b40924ebc7d1faf7385f6850e5c6bb
darlopvil <dariolopezvillegas97@gmail.com>
2021-03-13 21:07:59 +0100

    primer commit del video L04

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-
 var chad = require("cool-ascii-faces");
+var express = require("express");
 
-console.log(chad());
+var app = express();
+var port=10000;
+
+app.get("/chad", (req, res) => {
+    res.send(chad());
+    console.log("New request to /chad has arrived");
+});
+
+app.listen(port,()=>{
+    console.log("Server ready, listening on port "+ port);
+})
+
+
+//console.log(chad());
