70cdece97607ceb7beddb82aefe11dbb5484565d
darlopvil <dariolopezvillegas97@gmail.com>
2021-03-21 16:00:19 +0100

    cambios de mientras

--- l05/index.js
+++ l05/index.js
@@ -22,19 +22,91 @@ var puerto = (process.env.PORT || 10000); //la aplicacion arranca en el puerto e
 /*
 con .use(), partiendo de la raiz del programa, me busca en la carpeta "public"
 lo que sea que se haya pedido como petete.html o pedrito.jpg
+Comentado porque probé a hacer la subida a heroku
 */
 
-app.use("/", express.static("./public"));
+//app.use("/", express.static("./public"));
 
 //Acceso usando GET
 app.get("/hello", (req, res) => {
     res.send("<html><body><h1> GET Hello from this tiny server</h1></body></html>");
 });
 
-//Acceso usando POST
-app.post("/hello", (req, res) => {
-    res.send("<html><body><h1> POST Hello from this tiny server</h1></body></html>");
+//Acceso especial heroku edition
+app.get("/info/air_rutes",(req, res)=>{
+
+    res.send(`<!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+    table, th, td {
+      border: 1px solid black;
+    }
+    </style>
+    </head>
+    <body>
+    
+    <h3> <a href=" https://www.fomento.gob.es/BE/?nivel=2&orden=03000000"> Enlace de referencia </a>
+    </h3>
+      <table>
+      <colgroup>
+        <col style="background-color:#f2f2f2">
+      </colgroup>
+      <tr>
+        <th>districts</th>
+        <th>years</th>
+        <th>flights</th>
+        <th>passengers</th>
+        <th>total_goods(kg)</th>
+      </tr>
+      <tr>
+        <td>Madrid</td>
+        <td>2020</td>
+        <td>158405</td>
+        <td>17071089</td>
+        <td>401133380</td>
+      </tr>
+      <tr>
+        <td>Cataluña</td>
+        <td>2017</td>
+        <td>320456</td>
+        <td>47262688</td>
+        <td>156105304</td>
+      </tr>
+  
+      <tr>
+        <td>Andalucía</td>
+        <td>2020</td>
+        <td>78777</td>
+        <td>7441585</td>
+        <td>10561907</td>
+      </tr>
+  
+      <tr>
+        <td>Andalucía</td>
+        <td>2019</td>
+        <td>194370</td>
+        <td>27332163</td>
+        <td>12971632</td>
+      </tr>
+  
+      <tr>
+        <td>Madrid</td>
+        <td>2019</td>
+        <td>417958</td>
+        <td>61703370</td>
+        <td>558566726</td>
+      </tr>
+    </table>
+    
+    </body>
+    </html>`);
 });
+
+//Acceso usando POST -comentado por heroku-
+/*app.post("/hello", (req, res) => {
+    res.send("<html><body><h1> POST Hello from this tiny server</h1></body></html>");
+});*/
 
 //arrancar el server
 
